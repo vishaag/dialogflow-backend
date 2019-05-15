6612ad88226bc3e64c541045ff2a810fbbbab3b3
vishaag <vishaag@gmail.com>
2019-05-16 01:47:37 +0530

    change url path

--- index.py
+++ index.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 source = 'https://github.com/zeit/now-examples/tree/master/python-flask'
 css = '<link rel="stylesheet" href="/css/style.css" />'
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
+# @app.route('/', defaults={'path': ''})
+# @app.route('/<path:path>')
+# def catch_all(path):
+#     return Response("%s<h1>Flask on Now 2.0</h1><p>You are viewing a Flask application written in Python running on Now 2.0.</p><p>Visit the <a href='./about'>about</a> page or view the <a href='%s'>source code</a>.</p>" % (css, source), mimetype='text/html')
+
+@app.route('/')
+def index():
+    return "Hello, World!"
